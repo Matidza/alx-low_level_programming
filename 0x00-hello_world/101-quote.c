e3b12e8f4c490e32df00b729f32997c5b36b763f
Matidza <125007667+Matidza@users.noreply.github.com>
2023-07-13 23:57:46 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	putc("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+	fputc("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
 	return (1);
 }
