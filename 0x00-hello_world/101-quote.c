7b9725ca14b23644b7de6537cbf9efb7bea8861f
matidza <matidza46@gmail.com>
2023-07-13 20:01:12 +0200

    twelfth commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point of the program
  * Description: This function prints the size of various types
  * on the computer it is compiled and run on.
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
-	return (0);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+	return (1);
 }
