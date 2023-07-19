586c64dd5bb398c08cd169cb2eeff83a637414c7
matidza <matidza46@gmail.com>
2023-07-19 20:16:04 +0200

    This is hard

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,29 +1,15 @@
-#include <stdio.h>
-
-void print_to_98(int n) 
+#include "main.h"
+/**
+ * main - Entry point
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return: sum of a and b
+ */
+int add(int a, int b)
 {
-    if (n <= 98) {
-        for (int i = n; i <= 98; i++) {
-            std::cout << i;
-            if (i != 98) {
-                std::cout << ", ";
-            }
-        }
-    } else {
-        for (int i = n; i >= 98; i--) {
-            std::cout << i;
-            if (i != 98) {
-                std::cout << ", ";
-            }
-        }
-    }
-    std::cout << std::endl;
-}
+	int c;
 
-int main() {
-    int n;
-    std::cout << "Enter a number: ";
-    std::cin >> n;
-    print_to_98(n);
-    return 0;
+	c = a + b;
+
+	return (c);
 }
